fed81ce6174c029522c4d24c89df71a82a7330aa
Miyayayayayaya <s1f102502542@iniad.org>
2026-05-13 23:58:12 +0900

    記録

---
+++ ABC/456/a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int X;
+    cin >> X;
+    for (int i = 1; i <= 6; i++)
+    {
+        for (int j = 1; j <= 6; j++)
+        {
+            for (int k = 1; k <= 6; k++)
+            {
+                if (i + j + k == X)
+                {
+                    cout << "Yes" << endl;
+                }
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
+}
